bbdde1c58d0ecee2727b0d2279262a807f5fba91
parendu parikh <parendu parikh>
2017-05-10 00:44:24 -0400

    test

--- video.js
+++ video.js
@@ -10,6 +10,7 @@
 
 
 // Search for a specified string.
+$(function() {
 $("#search-button").on("click", function(event) {
    event.preventDefault();
   var q = $('#search-term').val();
@@ -26,16 +27,14 @@ $("#search-button").on("click", function(event) {
     console.log(str);
     $('#videos-appear-here').html('<pre>' + str + '</pre>');
   });
-
+});
 
 });
 
 function init(){
-<<<<<<< HEAD
+
        gapi.client.setApiKey("AIzaSyAs92o-m1w6elH20BhKZAIy0eggx8YCEmw");
-=======
-       gapi.client.setApiKey("AIzaSyBLYFa-iwmIdBWQWTGLOEIWFSfFqFFns0Q");
->>>>>>> ce836b358fc95f85e3a9aa2b7212e82dc506ba4b
+
        gapi.client.load("youtube", "v3", function(){
 
        });
