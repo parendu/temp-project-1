1e1eb676590d508fc1d5dfb27da1b7887a608cd4
parendu parikh <parendu parikh>
2017-05-10 18:09:35 -0400

    test

--- video.js
+++ video.js
@@ -13,42 +13,42 @@ function init(){
 
 };
 
-// //Get user entered search terms and assign to variable q
-// $("#search-button").on("click", function(event) {
-//    event.preventDefault();
-//    console.log("hello");
-//   var q = $('#search-term').val();
-//   console.log(q);
-  
-// });
+      //Get user entered search terms and assign to variable q
+      $("#search-button").on("click", function(event) {
+         event.preventDefault();
+         console.log("hello");
+        var q = $('#search-term').val();
+        console.log(q);
+        
+      });
 
-// //create function to send api request to youtube server
-// function search(){
-//  // console.log("q: "+ q);
-//   var request = gapi.client.youtube.search.list({
-//     q: 'india',
-//     part: 'snippet',
-//     type: "video",
-//     maxResults: 3,
-//     key: "AIzaSyAs92o-m1w6elH20BhKZAIy0eggx8YCEmw"
-    
-//   });
-   
-//    //send request to youtube server
-//     request.execute(onSearchResponse);
+      //create function to send api request to youtube server
+      function search(){
+       // console.log("q: "+ q);
+        var request = gapi.client.youtube.search.list({
+          q: 'india',
+          part: 'snippet',
+          type: "video",
+          maxResults: 3,
+          key: "AIzaSyAs92o-m1w6elH20BhKZAIy0eggx8YCEmw"
+          
+        });
+         
+         //send request to youtube server
+          request.execute(onSearchResponse);
 
-// };
+      };
 
-//create the function to display content   
-// function resultResponse(response) {
-   
-//      var str = JSON.stringify(response, '', 2);
-//     console.log(str);
-   
-//   $('#videos-appear-here').html('<pre>' + str + '</pre>');
+      create the function to display content   
+      function resultResponse(response) {
+         
+           var str = JSON.stringify(response, '', 2);
+          console.log(str);
+         
+        $('#videos-appear-here').html('<pre>' + str + '</pre>');
 
 
-//   };
+        };
 
 
 // // Called automatically with the response of the YouTube API request.
@@ -59,28 +59,4 @@ function init(){
 
 
  
-
-  $('#search-term').submit(function(event){
-    event.preventDefault();
-    var searchTerm = $('#search-term').val();
-    console.log(searchTerm);
-    
-
-
- var queryURL = "https://www.googleapis.com/youtube/v3/search?q=" + searchTerm + "&part=snippet"+"&key=AIzaSyAs92o-m1w6elH20BhKZAIy0eggx8YCEmw";
-    //Call ajax function
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function(response) {
-        //set animal-view att animal
-        console.log(response);
-        $("#animal-view").attr(animal);
-
-        //set results with resopnse data
-        var results = response.data;
-        console.log("resoponse" + results);
-
-      });
-  });
   
