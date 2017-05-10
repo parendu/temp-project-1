fc4b739b8772b455c8d3d3908ec58d6206ac1d89
parendu parikh <parendu parikh>
2017-05-10 15:45:55 -0400

    test

--- video.js
+++ video.js
@@ -1,17 +1,17 @@
 
 
-// //ini function to load youtube api library and set apikey
-// function init(){
+//ini function to load youtube api library and set apikey
+function init(){
 
-//        gapi.client.setApiKey("AIzaSyAs92o-m1w6elH20BhKZAIy0eggx8YCEmw");
+       gapi.client.setApiKey("AIzaSyAs92o-m1w6elH20BhKZAIy0eggx8YCEmw");
 
-//        gapi.client.load("youtube", "v3", function(){
+       gapi.client.load("youtube", "v3", function(){
 
-//        search();               
+       search();               
 
-//        });
+       });
 
-// };
+};
 
 // //Get user entered search terms and assign to variable q
 // $("#search-button").on("click", function(event) {
@@ -66,51 +66,19 @@
     search(searchTerm);
   });
 
-// Your use of the YouTube API must comply with the Terms of Service:
-// https://developers.google.com/youtube/terms
 
-// Helper function to display JavaScript value on HTML page.
+ var queryURL = "https://www.googleapis.com/youtube/v3/search?q=" + searchTerm + "&part=snippet"+"&key=AIzaSyAs92o-m1w6elH20BhKZAIy0eggx8YCEmw";
+    //Call ajax function
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+        //set animal-view att animal
+        console.log(response);
+        $("#animal-view").attr(animal);
 
-function showResponse(response) {
-    var responseString = JSON.stringify(response, '', 2);
-    document.getElementById('videos-appear-here').innerHTML += responseString;
-}
+        //set results with resopnse data
+        var results = response.data;
+        console.log("resoponse" + results);
 
-// Called automatically when JavaScript client library is loaded.
-function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-
-// Called automatically when YouTube API interface is loaded (see line 9).
-function onYouTubeApiLoad() {
-    // This API key is intended for use only in this lesson.
-    // See https://goo.gl/PdPA1 to get a key for your own applications.
-    gapi.client.setApiKey('AIzaSyAs92o-m1w6elH20BhKZAIy0eggx8YCEmw');
-
-    search();
-}
-
-function search() {
-    // Use the JavaScript client library to create a search.list() API call.
-    var q = $('#search-term').val();
-    var request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        q: q,
-        maxResults: 3
-       
-        
-    });
-    
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
-     request.execute(function(response) {
-    var str = JSON.stringify(response.result);
-    $('#videos-appear-here').html('<pre>' + str + '</pre>');
   });
-}
-
-// Called automatically with the response of the YouTube API request.
-   function onSearchResponse(response) {
-     showResponse(response);
-}
-});
