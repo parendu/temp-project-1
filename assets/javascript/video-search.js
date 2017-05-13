9228fcbdc668c12f3f7084ec7d2f1d420759e05b
parendu parikh <parendu parikh>
2017-05-13 13:26:31 -0400

    iframe copy from youtube

--- assets/javascript/video-search.js
+++ assets/javascript/video-search.js
@@ -209,7 +209,7 @@ $(document).ready(function(){
                 var href = $(this).attr('data-href');
                  console.log("src: " + href);
                 // maybe use an ID instead
-                $('embed').attr('src', href);
+                $('.youtube-player').attr('src', href);
              });
      
 
