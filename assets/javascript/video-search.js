b5c565b513d58ab515283ef21b85269a6b7dc065
parendu parikh <parendu parikh>
2017-05-15 14:18:26 -0400

    favorite code added

--- assets/javascript/video-search.js
+++ assets/javascript/video-search.js
@@ -197,9 +197,76 @@
    $(document).on('click', '#favButton', function(e) {
          console.log("clicked")
          e.preventDefault();
+
+
+                $('#Favorite-video').addClass('bordered');
+
+                 //create class image for each video        
+                 var videoDiv = $("<div class = 'col-sm-3 item' margin-bottom='10px'>");
+
+                 //create favorite buttton
+                 var favButton = $("<button type='button' id='favButton' value='click' margin-bottom='10px' class='btn btn-default btn-sm favorite'>");
+
+                 var addSpan = $("<span>");
+                 addSpan.addClass('glyphicon glyphicon-star')
+                 addSpan.attr("aria-hidden", 'true');
+
+                 //create play buttton
+
+
+                 var playButton = $("<button type='button'  name=" + videoId + "value='click' margin-bottom='10px' class='btn btn-default btn-sm play-button'>");
+                 var playSpan = $("<span>");
+                 playSpan.addClass('glyphicon glyphicon-play')
+                 playSpan.attr("aria-hidden", 'true');
+
+                 //adding href to span
+
+                 playButton.attr("data-href", 'https://www.youtube.com/embed/' + videoId);
+
+                 //create image element
+                 var videoThumb = $("<div class='thumbnail'>");
+
+                 var videoImage = $("<img>");
+
+                 //add class to image
+                 videoImage.addClass('video-image');
+
+                 //set src image
+                 //set data-videoid
+                 videoImage.attr("src", imageUrl);
+                 videoImage.attr("data-videoId", videoId);
+                 videoImage.attr("value", "click");
+                 videoImage.attr("data-href", 'https://www.youtube.com/embed/' + videoId);
+
+                 // videoImage.attr("href", 'https://www.youtube.com/watch?v='+videoId);
+                 //videoImage.append("<a href=https://www.youtube.com/watch?v="+ videoId + " target='video-play'");
+
+
+                 //prepend  video image to videoDiv
+
+                 videoThumb.append(videoImage);
+                 favButton.append(addSpan);
+
+                 //playButton.append(playA);
+                 playButton.append(playSpan);
+
+
+                 videoThumb.prepend(favButton);
+                 videoThumb.prepend(playButton);
+                 videoThumb.append(p);
+                 videoDiv.prepend(videoThumb);
+
+                 //  mouseover display full title
+                 // $( ".thumbnail" )
+                 //      .mouseover(function() {
+                 //    $( this ).find( "p" ).text(videoTitleFull);
+                 //  })
+
+                 $("Favorite-appear-here").prepend(videoDiv);
+
+
         
-         var thumbnail = $(this).append("#thumbnail");
-         console.log(thumbnails);
+       
           
     });     
 
