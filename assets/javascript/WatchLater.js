2df13f776dec822a289bb5975bfe19f0e16c74fe
parendu parikh <parendu parikh>
2017-05-15 14:00:56 -0400

    favorite code added

---
+++ assets/javascript/WatchLater.js
@@ -0,0 +1,13 @@
+
+
+ $(document).ready(function() {
+
+     //capture search-term text box when user click search-button
+     $("#favorite").on("click", function(event) {
+         event.preventDefault();
+         console.log("hello");
+         var thumbnail = $(this).append("#thumbnail");
+         console.log(thumbnails);
+          
+         
+ }); //document.ready
