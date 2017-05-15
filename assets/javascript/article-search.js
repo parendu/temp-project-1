f8b7f76a3d9a0b853f4e0ad1ce6ba15ce368fe6f
parendu parikh <parendu parikh>
2017-05-14 21:58:31 -0400

    articles and article_search.js

--- assets/javascript/article-search.js
+++ assets/javascript/article-search.js
@@ -4,45 +4,45 @@
 
      var searchTerm = $('#search-term').val();
      console.log(searchTerm);
-        // The Base URL
-        var queryUrl = "https://newsapi.org/v1/articles?source=ars-technica&sortBy=top&apiKey=2d7535a7fe9c4f85a3ecff0e4993a567";
+     // The Base URL
+     var queryUrl = "https://newsapi.org/v1/articles?source=ars-technica&sortBy=top&apiKey=2d7535a7fe9c4f85a3ecff0e4993a567";
 
-        // putting the url togehter and append &callback=?
-        var url = queryUrl;
-        console.log(url);
+     // putting the url togehter and append &callback=?
+     var url = queryUrl;
+     console.log(url);
 
-        // Call The API for a JSON
-        $.getJSON(url, function() {
-            console.log("success");
-        }).done(function(response) {
-                console.log("second success");
+     // Call The API for a JSON
+     $.getJSON(url, function() {
+         console.log("success");
+     }).done(function(response) {
+         console.log("second success");
 
-                console.log(response.articles);
+         console.log(response.articles);
 
-                //var results = response.articles;
-                
-                console.log(response.articles);
+         //var results = response.articles;
 
-                // create a var for the results and append a header
-                var results = '<li data-role="list-divider">Results</li>';
-               
-                for (var i = 0; i < response.articles.length; i++) {
-                    
-                    results += '<li>';
-                    results += response.articles[i].title;
-                    results += '</li>';
-                };  //loop
-                 
-                // // clear the results . append the re sults .refresh the listview
-                $('#results').empty().append(results);
+         console.log(response.articles);
 
-                  // }).fail(function() {
-                  //   console.log("error");
-                  //  }).always(function() {
-                  //  console.log("always");
-                  //  });
+         // create a var for the results and append a header
+         var results = '<li data-role="list-divider">Results</li>';
 
-            }); //response
-        }; //if
+         for (var i = 0; i < response.articles.length; i++) {
 
-});
+             results += '<li>';
+             results += response.articles[i].title;
+             results += '</li>';
+         }; //loop
+
+         // // clear the results . append the re sults .refresh the listview
+         $('#results').empty().append(results);
+
+         // }).fail(function() {
+         //   console.log("error");
+         //  }).always(function() {
+         //  console.log("always");
+         //  });
+
+     }); //response
+   
+
+ });
