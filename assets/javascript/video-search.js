e8f92f3a5f089610ae2173e452279e0ad90ba02d
parendu parikh <parendu parikh>
2017-05-15 19:46:33 -0400

    window reload

--- assets/javascript/video-search.js
+++ assets/javascript/video-search.js
@@ -22,6 +22,7 @@
           $(".youtube-player").empty();
           $("#video-display").removeClass("bordered");
           $("#video-appear-here").empty();
+          window.location.reload();
 
          console.log("hello");
          var searchTerm = $('#search-term').val();
