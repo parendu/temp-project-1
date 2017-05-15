2750697f1a9986529caac15e507eb945e6d4e97e
parendu parikh <parendu parikh>
2017-05-14 22:56:12 -0400

    making articles as link

--- assets/javascript/article-search.js
+++ assets/javascript/article-search.js
@@ -5,7 +5,7 @@
      var searchTerm = $('#search-term').val();
      console.log(searchTerm);
      // The Base URL
-     var queryUrl = "https://newsapi.org/v1/articles?source=ars-technica&sortBy=top&apiKey=2d7535a7fe9c4f85a3ecff0e4993a567";
+     var queryUrl = "https://newsapi.org/v1/sources?language=en&category=technology&sortBy=top&apiKey=2d7535a7fe9c4f85a3ecff0e4993a567";
 
      // putting the url togehter and append &callback=?
      var url = queryUrl;
@@ -17,21 +17,32 @@
      }).done(function(response) {
          console.log("second success");
 
-         console.log(response.articles);
+         console.log(response.sources);
 
          //var results = response.articles;
 
-         console.log(response.articles);
+         console.log(response.sources);
 
          // create a var for the results and append a header
          var results = '<li data-role="list-divider"><strong>Articles:</strong</li>';
-
-         for (var i = 0; i < response.articles.length; i++) {
+         //create news articles list creating li attribute
+         for (var i = 0; i < response.sources.length; i++) {
 
              results += '<li>';
-             results += response.articles[i].title;
-             results += response.articles[i].url;
+             results += '<a href=' + response.sources[i].url + 'name='+ response.sources[i].description+'</a>';
+            // results += response.sources[i].description+'<br>';
+             //results += response.sources[i].url;
              results += '</li>';
+
+a = document.createElement('a');
+    a.href =  'google.com'; // Insted of calling setAttribute 
+    a.innerHTML = "Link" // <a>INNER_TEXT</a>
+    leftDiv.appendChild(a); // Append the link to the div
+    document.body.appendChild(leftDiv); // And append the div to the document body
+
+
+
+
          }; //loop
           
           //add border
