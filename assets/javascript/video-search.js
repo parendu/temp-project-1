5e3ff3986be4bdfb347ae7e88661b77ac2ef5ab5
parendu parikh <parendu parikh>
2017-05-15 20:17:27 -0400

    updated js path to reload

--- assets/javascript/video-search.js
+++ assets/javascript/video-search.js
@@ -22,7 +22,7 @@
           $(".youtube-player").empty();
           $("#video-display").removeClass("bordered");
           $("#video-appear-here").empty();
-          window.location.reload(true);
+         // window.location.reload(true);
 
          console.log("hello");
          var searchTerm = $('#search-term').val();
