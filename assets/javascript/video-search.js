fa4e2d25c782342bd8308f4ed018e4bb795707ae
parendu parikh <parendu parikh>
2017-05-16 22:20:53 -0400

    watch later code removed from video-search

--- assets/javascript/video-search.js
+++ assets/javascript/video-search.js
@@ -186,96 +186,97 @@
          $('iframe').attr('src', href);
      });
 
+/////////////////////////////////////////////////////////////
 // click on watchlater button and get value
 
- 
+////////////////////////////////////////////////////////// 
 
-     //capture search-term text box when user click favorite-button
-   $(document).on('click', '.favorite', function(e) {
-         console.log("clicked")
-         e.preventDefault();
+   //   //capture search-term text box when user click favorite-button
+   // $(document).on('click', '.favorite', function(e) {
+   //       console.log("clicked")
+   //       e.preventDefault();
           
                   
-          //get the data videoId, imageUrl and videoTitle from results
-              var videoId = $(this).attr('id');
-              var videoId = $("id").val()
-              console.log(videoId);               
-         // console.log(videoId);
-                 var imageUrl = $(this).attr('src');
-                 var imageUrl = $("src").val();
-                 console.log(imageUrl);
+   //        //get the data videoId, imageUrl and videoTitle from results
+   //            var videoId = $(this).attr('id');
+   //            var videoId = $("id").val()
+   //            console.log(videoId);               
+   //       // console.log(videoId);
+   //               var imageUrl = $(this).attr('src');
+   //               var imageUrl = $("src").val();
+   //               console.log(imageUrl);
                  
                 
-                 var p = $("<p>").text("videoTitleFull");
-                  console.log(p);
+   //               var p = $("<p>").text("videoTitleFull");
+   //                console.log(p);
 
-                $('#Favorite-video').addClass('bordered');
+   //              $('#Favorite-video').addClass('bordered');
 
-                 //create class image for each video        
-                 var videoDiv = $("<div class = 'col-sm-3 item' margin-bottom='10px'>");
+   //               //create class image for each video        
+   //               var videoDiv = $("<div class = 'col-sm-3 item' margin-bottom='10px'>");
 
-                 //create favorite buttton
-                 var favButton = $("<button type='button' id='favButton' value='click' margin-bottom='10px' class='btn btn-default btn-sm favorite'>");
+   //               //create favorite buttton
+   //               var favButton = $("<button type='button' id='favButton' value='click' margin-bottom='10px' class='btn btn-default btn-sm favorite'>");
 
-                 var addSpan = $("<span>");
-                 addSpan.addClass('glyphicon glyphicon-star')
-                 addSpan.attr("aria-hidden", 'true');
+   //               var addSpan = $("<span>");
+   //               addSpan.addClass('glyphicon glyphicon-star')
+   //               addSpan.attr("aria-hidden", 'true');
 
-                 //create play buttton
-
-
-                 var playButton = $("<button type='button'  name=" + videoId + "value='click' margin-bottom='10px' class='btn btn-default btn-sm play-button'>");
-                 var playSpan = $("<span>");
-                 playSpan.addClass('glyphicon glyphicon-play')
-                 playSpan.attr("aria-hidden", 'true');
-
-                 //adding href to span
-
-                 playButton.attr("data-href", 'https://www.youtube.com/embed/' + videoId);
-
-                 //create image element
-                 // var videoThumb = $("<div class='thumbnail'>");
-
-                 var videoImage = $("<img>");
-
-                 //add class to image
-                 videoImage.addClass('video-image');
-
-                 //set src image
-                 //set data-videoid
-                 videoImage.attr("src", imageUrl);
-                 videoImage.attr("data-videoId", videoId);
-                 videoImage.attr("value", "click");
-                 videoImage.attr("data-href", 'https://www.youtube.com/embed/' + videoId);
-
-                 // videoImage.attr("href", 'https://www.youtube.com/watch?v='+videoId);
-                 //videoImage.append("<a href=https://www.youtube.com/watch?v="+ videoId + " target='video-play'");
+   //               //create play buttton
 
 
-                 //prepend  video image to videoDiv
+   //               var playButton = $("<button type='button'  name=" + videoId + "value='click' margin-bottom='10px' class='btn btn-default btn-sm play-button'>");
+   //               var playSpan = $("<span>");
+   //               playSpan.addClass('glyphicon glyphicon-play')
+   //               playSpan.attr("aria-hidden", 'true');
 
-                 videoThumb.append(videoImage);
-                 favButton.append(addSpan);
+   //               //adding href to span
 
-                 //playButton.append(playA);
-                 playButton.append(playSpan);
+   //               playButton.attr("data-href", 'https://www.youtube.com/embed/' + videoId);
+
+   //               //create image element
+   //               // var videoThumb = $("<div class='thumbnail'>");
+
+   //               var videoImage = $("<img>");
+
+   //               //add class to image
+   //               videoImage.addClass('video-image');
+
+   //               //set src image
+   //               //set data-videoid
+   //               videoImage.attr("src", imageUrl);
+   //               videoImage.attr("data-videoId", videoId);
+   //               videoImage.attr("value", "click");
+   //               videoImage.attr("data-href", 'https://www.youtube.com/embed/' + videoId);
+
+   //               // videoImage.attr("href", 'https://www.youtube.com/watch?v='+videoId);
+   //               //videoImage.append("<a href=https://www.youtube.com/watch?v="+ videoId + " target='video-play'");
 
 
-                 videoThumb.prepend(favButton);
-                 videoThumb.prepend(playButton);
-                 videoThumb.append(p);
-                 videoDiv.prepend(videoThumb);
+   //               //prepend  video image to videoDiv
 
-                 //  mouseover display full title
-                 // $( ".thumbnail" )
-                 //      .mouseover(function() {
-                 //    $( this ).find( "p" ).text(videoTitleFull);
-                 //  })
+   //               videoThumb.append(videoImage);
+   //               favButton.append(addSpan);
 
-                 $("Favorite-appear-here").prepend(videoDiv);      
+   //               //playButton.append(playA);
+   //               playButton.append(playSpan);
+
+
+   //               videoThumb.prepend(favButton);
+   //               videoThumb.prepend(playButton);
+   //               videoThumb.append(p);
+   //               videoDiv.prepend(videoThumb);
+
+   //               //  mouseover display full title
+   //               // $( ".thumbnail" )
+   //               //      .mouseover(function() {
+   //               //    $( this ).find( "p" ).text(videoTitleFull);
+   //               //  })
+
+   //               $("Favorite-appear-here").prepend(videoDiv);      
        
           
-    });     
+   //  });     
 
 
 
