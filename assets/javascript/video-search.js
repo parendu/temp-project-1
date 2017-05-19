333b98722d2c5f766d346e8c62b3cce8321bfc8d
parendu parikh <parendu parikh>
2017-05-19 10:03:47 -0400

    fixing tab navigation - added video-search.js code

--- assets/javascript/video-search.js
+++ assets/javascript/video-search.js
@@ -154,19 +154,16 @@
      //displaying correct content on tabs navigation
      
      // tab
-             $('#myTab a').click(function(e) {
-              e.preventDefault();
-              $(this).tab('show');
+                    
+                if (location.hash) {
+                    $("a[href='" + location.hash + "']").tab("show");
+                }
+                $(document.body).on("click", "a[data-toggle]", function(event) {
+                    location.hash = this.getAttribute("href");
+                });
             });
-
-            // store the currently selected tab in the hash value
-            $("ul.nav-tabs > li > a").on("shown.bs.tab", function(event) {
-              var id = $(event.target).attr("href").substr(1);
-              window.location.hash = id;
-            });
-
-            // on load of the page: switch to the currently selected tab
-            var hash = window.location.hash;
-            $('#myTab a[href="' + hash + '"]').tab('show');
-
+            $(window).on("popstate", function() {
+                var anchor = location.hash || $("a[data-toggle='tab']").first().attr("href");
+                $("a[href='" + anchor + "']").tabs("show");
+          
  }); //document.ready
